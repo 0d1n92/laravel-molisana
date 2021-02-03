eef3e60c7534e6a926a29e1f4a19184b04d40475
enrico <rombaldonienrico@gmail.com>
2021-02-03 23:02:54 +0100

    footer_not_completed

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,42 @@
+<footer>
+ <div class="margin_15">
+    <div class="flex_row">
+
+      <ul id="info">
+        <li><a href=""><img id="logo_footer" src="img/logo.png" alt=""></a></li>
+        <li>Ragione sociale: La Molisana S.P.A.</li>
+        <li>Sede legale: contrada Colle delle Api, 100/A</li>
+        <li>86100 - Campobasso (CB)</li>
+        <li>Pec: lamolisana@pec.it</li>
+        <li>Tel: +39 0874 4981</li>
+        <li>Fax: +39 0874 629584</li>
+        <li>ingo@lamolisana.it (per segnalazioni degli utenti)</li>
+        <li>commerciale@lamolisana.it</li>
+        <li>export@lamolisana.it</li>
+        <li>numero verde 800818081</li>
+        <li>telefono 3801292455</li>
+      </ul>
+      <ul>
+        <li><a href="#">Il Pastificio</a></li>
+        <li><a href="#">Grano decorticato a pietra</a></li>
+        <li><a href="#">Il Molise c'è</a></li>
+        <li><a href="#">Filiera integrata</a></li>
+        <li><a href="#">100 anni di pasta</a></li>
+        <li><a href="#">Sartoria della pasta</a></li>
+        <li><a href="#">Spaghetto quadrato</a></li>
+        <li><a href="#">Le persone</a></li>
+      </ul>
+      <ul>
+        <li><a href="#">Il Pastificio</a></li>
+        <li><a href="#">Grano decorticato a pietra</a></li>
+        <li><a href="#">Il Molise c'è</a></li>
+        <li><a href="#">Filiera integrata</a></li>
+        <li><a href="#">100 anni di pasta</a></li>
+        <li><a href="#">Sartoria della pasta</a></li>
+        <li><a href="#">Spaghetto quadrato</a></li>
+        <li><a href="#">Le persone</a></li>
+      </ul>
+
+    </div>
+  </div> 
+</footer>
